cfbcd25ee4283e86a78e7a2f7bb4641b0f2c6504
nachiPatel <nachi.patel97@gmail.com>
2026-01-21 12:18:38 +0530

    cleaning up

--- README.md
+++
@@ -1,2 +0,0 @@
-# b2b-ecom-api
-backend api for b2b ecom site.
--- prisma/migrations/20251125130328_add_packaging_to_order_item/migration.sql
+++
@@ -1,3 +0,0 @@
--- AlterTable
-ALTER TABLE "OrderItem" ADD COLUMN     "packagingName" TEXT,
-ADD COLUMN     "unitsPerPackage" INTEGER;
